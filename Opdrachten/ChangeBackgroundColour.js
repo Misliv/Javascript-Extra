6593cf7feebb1f1350fcc7e54093fa2640df6436
May <mayandmays@hotmail.com>
2021-03-31 11:17:02 +0200

    moved to a folder, background colour changer af,

---
+++ Opdrachten/ChangeBackgroundColour.js
@@ -0,0 +1,12 @@
+let body = document.getElementById("body");
+let Hexdecimaal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'A', 'B' , 'C' ,'D', 'E', 'F'];
+
+function Kleur () {
+    let hex = "#";
+    for (let i = 0; i < 6; i++) {
+        let index = Math.floor(Math.random()*Hexdecimaal.length);
+        hex += Hexdecimaal[index];
+    }
+    document.getElementById("Hex").innerText = "Hex Colour: " + hex;
+    document.body.style.backgroundColor = hex;
+}
