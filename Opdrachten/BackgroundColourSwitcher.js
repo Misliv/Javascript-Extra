c772f1cd667398d8cd1431b07c0174059e93c3e1
may.hoang@student.rocvf.nl <H0an91123581321>
2021-04-06 23:47:34 +0200

    most of the files are done, looks bad tho, imageslider doesnt have working arrows

---
+++ Opdrachten/BackgroundColourSwitcher.js
@@ -0,0 +1,15 @@
+document.getElementById("Red").addEventListener("click", function () {
+    document.body.style.backgroundColor = "red"
+});
+
+document.getElementById("Green").addEventListener("click", function () {
+    document.body.style.backgroundColor = "green"
+});
+
+document.getElementById("Blue").addEventListener("click", function () {
+    document.body.style.backgroundColor = "blue"
+});
+
+document.getElementById("Black").addEventListener("click", function () {
+    document.body.style.backgroundColor = "black"
+});
