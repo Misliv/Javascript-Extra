b9467b21ed34908ff802d0a925aa030664112c54
May <mayandmays@hotmail.com>
2021-04-14 10:08:01 +0200

    new files

---
+++ 12.6.js
@@ -0,0 +1,7 @@
+let values = ["1", "1", "1", "2", "2", "3", "3", "4", "5","5", "6", "7", "7", "7", "8", "8", "9", "9", "9", "9"]
+
+function getValue() {
+    document.getElementById("value").innerText = "Value: " + (values);
+    document.getElementById("value").innerText = "Amount of times shown: " + (values);
+
+}
