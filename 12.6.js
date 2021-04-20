64469fcfc2497c6f05aa605c7da181f540856e8b
may.hoang@student.rocvf.nl <H0an91123581321>
2021-04-20 22:22:50 +0200

    few tests

--- 12.6.js
+++ 12.6.js
@@ -1,7 +1,15 @@
 let values = ["1", "1", "1", "2", "2", "3", "3", "4", "5","5", "6", "7", "7", "7", "8", "8", "9", "9", "9", "9"]
+let nummer
+let count
+
 
 function getValue() {
-    document.getElementById("value").innerText = "Value: " + (values);
-    document.getElementById("value").innerText = "Amount of times shown: " + (values);
 
+    for (let teller = 0; teller < values.length; teller++) {
+        if (nummer == values[teller]) {
+            count++;
+        }
+    }
+    document.getElementById("value").innerText = "Value: " + nummer;
+    document.getElementById("value").innerText = "Amount of times shown: " + count;
 }
